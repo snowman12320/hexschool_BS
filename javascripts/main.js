8fe0a00fc86c4d1ae1f9826e3236a45b7824515f
DESKTOP-QIDODUT\acer <snowman12320@gmail.com>
2022-10-06 20:30:12 +0800

    完成：側欄

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,5 +2,5 @@ const toggleMenuBtn = document.querySelector('#toggle-btn');
 const body = document.querySelector('body');
 toggleMenuBtn.addEventListener('click',(e) => {
     e.preventDefault();
-    body.classList.toggle('.sidebar-toggled');
+    body.classList.toggle('sidebar-toggled');
 });
