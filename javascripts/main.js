2dec143b3044410f13fea8d85db20dcfe2715bc6
DESKTOP-QIDODUT\acer <snowman12320@gmail.com>
2022-10-10 16:26:45 +0800

    首頁：完成+RWD

--- javascripts/main.js
+++ javascripts/main.js
@@ -16,7 +16,7 @@ function unhover_custom(element) {
   element.setAttribute("src", `./nbaWeb/silhouette-basket-01.svg`);
 }
 
-//最新產品圖
+//產品圖
 $(document).ready(function () {
   $(".card-img").mouseover(function (e) {
     var choose = e.target.dataset.num;
@@ -28,17 +28,16 @@ $(document).ready(function () {
   });
 });
 
-//排名圖示
-function hover_rank(element) {
-  element.setAttribute(
-    "src",
-    `../nbaWeb/olivier-collet-H7cIqigZOBo-unsplash.jpg`
-  );
-//   element.setAttribute("class", `of-cover`);
-}
-function unhover_rank(element) {
-  element.setAttribute("src", `../nbaWeb/PNG-images-NBA-Players-41png.png`);
-  element.removeAttribute("class", `of-cover`);
-}
+//排行圖
+$(document).ready(function () {
+  $(".card-img-top").mouseover(function (e) {
+    var choose = e.target.dataset.num;
+    $("body [data-num=" + choose + "]").addClass("hover_img");
+  });
+  $(".card-img-top").mouseout(function (e) {
+    var choose = e.target.dataset.num;
+    $("body [data-num=" + choose + "]").removeClass("hover_img");
+  });
+});
 
-// of-cover op-center
+
