16e5e5d25b0899c83e6ac74ad234ff25ebf55c18
DESKTOP-QIDODUT\acer <snowman12320@gmail.com>
2022-10-11 17:59:51 +0800

    完成：客制訂單

--- javascripts/main.js
+++ javascripts/main.js
@@ -40,3 +40,28 @@ $(document).ready(function () {
   //   $("body [data-num=" + choose + "]").removeClass("hover_img");
   // });
 });
+
+//註冊驗證
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+  "use strict";
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.querySelectorAll(".needs-validation");
+
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms).forEach(function (form) {
+    form.addEventListener(
+      "submit",
+      function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+
+        form.classList.add("was-validated");
+      },
+      false
+    );
+  });
+})();
